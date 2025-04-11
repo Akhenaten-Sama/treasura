0023e41aeb21cd3fcee39031774b2ff1c6de4d69
Olalekan <efunkunleolalekan@gmail.com>
2025-04-11 13:20:48 +0100

    added wallets module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { WalletsModule } from './wallets/wallets.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     AuthModule,
+    WalletsModule,
     UsersModule,
   ],
   controllers: [AppController],
